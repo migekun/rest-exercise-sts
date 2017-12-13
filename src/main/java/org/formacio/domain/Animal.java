05c28ac2d233a9a955c85852a9f30ebd1051a198
Miguel Angel Navas <manavas@gmail.com>
2017-12-13 10:09:50 +0100

    Exercici setmana 4

---
+++ src/main/java/org/formacio/domain/Animal.java
@@ -0,0 +1,67 @@
+package org.formacio.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_animals")
+public class Animal {
+	
+	/**
+	 * L'id d'animal es el propi nom. No fa falta cap generador
+	 */
+	@Id
+	@Column(name="ani_nom")
+	private String nom;
+	
+	@ManyToOne
+	@JoinColumn(name="ani_propietari")
+	private Persona propietari;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public Persona getPropietari() {
+		return propietari;
+	}
+
+	public void setPropietari(Persona propietari) {
+		this.propietari = propietari;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Animal))
+			return false;
+		Animal other = (Animal) obj;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		return true;
+	}
+	
+	
+}
---
+++ src/main/java/org/formacio/domain/Casa.java
@@ -0,0 +1,66 @@
+package org.formacio.domain;
+
+
+public class Casa {
+
+	/**
+	 * Casa ha d'emprar l'estrategia per l'id IDENTITY
+	 */
+	
+    private Long id;
+	
+	private String adreca;
+	
+	private Municipi municipi;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAdreca() {
+		return adreca;
+	}
+
+	public void setAdreca(String adreca) {
+		this.adreca = adreca;
+	}
+
+	public Municipi getMunicipi() {
+		return municipi;
+	}
+
+	public void setMunicipi(Municipi municipi) {
+		this.municipi = municipi;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Casa))
+			return false;
+		Casa other = (Casa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
