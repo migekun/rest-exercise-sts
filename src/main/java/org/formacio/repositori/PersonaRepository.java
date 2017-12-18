46dee697b6704e651dd544c199e03dc88556ffd5
Miguel Angel Navas <manavas@gmail.com>
2017-12-18 08:11:19 +0100

    Realitzat darrer test

--- src/main/java/org/formacio/repositori/PersonaRepository.java
+++ src/main/java/org/formacio/repositori/PersonaRepository.java
@@ -4,7 +4,9 @@ import org.formacio.domain.Persona;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "persones", path = "persona")
 public interface PersonaRepository extends CrudRepository<Persona, Long> {
 
 	@Query(value="select persona from Persona persona join fetch persona.propietats where persona.id = :id")
